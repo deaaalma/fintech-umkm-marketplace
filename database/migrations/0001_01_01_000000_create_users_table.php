ecd65ffbf8d344dfa50c470d193c2951125c764f
deaaalma <hopegekdea@gmail.com>
2026-05-06 11:17:35 +0800

    feat: add phone and role fields to User model and migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone');
+            $table->enum('role', ['superadmin', 'admin_umkm', 'worker', 'customer'])->default('customer');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
