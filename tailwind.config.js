64e89de1acff12dd10ae587a476068990fc36616
deaaalma <hopegekdea@gmail.com>
2026-05-08 10:34:55 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,39 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue', // Kalau pakai Vue
-        './resources/js/**/*.jsx', // Kalau pakai React
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue", // Kalau pakai Vue
+        "./resources/js/**/*.jsx", // Kalau pakai React
         // 👇 TAMBAHKAN BARIS INI (Wajib buat Flowbite)
-        "./node_modules/flowbite/**/*.js"
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Hind Vadodara", ...defaultTheme.fontFamily.sans],
+                serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+            },
+            colors: {
+                primary: {
+                    DEFAULT: "#005F02", // Deep Forest Green
+                    light: "#427A43", // Sage Green (Secondary)
+                },
+                accent: "#C0B87A", // Sand Gold
+                neutral: {
+                    cream: "#F2E3BB", // Soft Cream (Background)
+                },
             },
         },
     },
 
     plugins: [
-        require('@tailwindcss/forms'),
+        require("@tailwindcss/forms"),
         // 👇 TAMBAHKAN PLUGIN INI (Wajib buat Flowbite)
-        require('flowbite/plugin')
+        require("flowbite/plugin"),
     ],
 };
